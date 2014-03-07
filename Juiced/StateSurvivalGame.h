23b4ddde745585a8b4244ae6a774d803c69ade46
Jeffrey Chau <chaujeff@usc.edu>
2014-03-07 15:13:08 -0800

    Added in game mode and overwrite for Red Particle

---
+++ Juiced/StateSurvivalGame.h
@@ -0,0 +1,13 @@
+//
+//  StateSurvivalGame.h
+//  Juiced
+//
+//  Created by Jeffrey on 3/7/14.
+//  Copyright (c) 2014 Silly Landmine Studios. All rights reserved.
+//
+
+#import "GameMode.h"
+
+@interface StateSurvivalGame : GameMode
+
+@end
---
+++ Juiced/StateSurvivalGame.m
@@ -0,0 +1,13 @@
+//
+//  StateSurvivalGame.m
+//  Juiced
+//
+//  Created by Jeffrey on 3/7/14.
+//  Copyright (c) 2014 Silly Landmine Studios. All rights reserved.
+//
+
+#import "StateSurvivalGame.h"
+
+@implementation StateSurvivalGame
+
+@end
